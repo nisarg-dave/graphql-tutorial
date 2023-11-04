d8104e6bf66875b285dc693e3939daccca5b6840
Nisarg Dave <ndave630@gmail.com>
2023-11-04 11:56:13 +0800

    learnt about resolver functions

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
 
 // ApolloServer is used to setup the server and conifgure it and tell Apollo how handle queries. Standalone is used to start listening for requests
+
+// resolvers are functions that return the data
+const resolvers = {
+  // Resolver function for Query type
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
+
+// If use asks for only the title in the games array then Apollo will handle that and only return the titles and leave out rest of data
 
 // server setup
 // Apollo server takes object as argumnet with two properties
@@ -10,6 +29,7 @@ const server = new ApolloServer({
   // typeDefs - defintions of types of data that we want to expose -> definition of the types, the queries we can make on them and relation to other types combine to form a Schema
   typeDefs,
   // resolvers
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
--- schema.js
+++ schema.js
@@ -15,7 +15,7 @@ export const typeDefs = `#graphql
         verified: Boolean!
     }
     type Query {
-        reviews: [Reviews]
+        reviews: [Review]
         games:  [Game]
         authors: [Author]
     }
